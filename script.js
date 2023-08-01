5e2d6f23c0bf7f76614d711b8aab7888a018bd7a
tkyzmina <tkyzmina@gmail.com>
2023-08-01 14:27:33 +0300

    (feat): add updated solution

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // написать функцию, которая будет считать до 50 каждые 300 милисекунд. после достижения 50. функция должна вернуть сообщение done
-// task update  - каждые 300 мс в счетчик увеличивается на 1 единицу. Работа заканчифается после достижения счетчиокм значения 50.
-const counter = async () => {
-  const res = setInterval(() => {
-    let counter = 0
-    while (counter <= 50) {
-      counter++
-      if (counter === 50) {
-        console.log('done')
-      }
-    }
-  }, 300)
-
-  setTimeout(() => {
-    clearInterval(res)
-  }, 3000) //думаю, 10 раз достаточно.
+// task update  - каждые 300 мс счетчик увеличивается на 1 единицу. Работа заканчивается после достижения счетчиком значения 50.
+const counter = async (value) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value >= 50 ? 'Done' : false)
+    }, 300)
+  })
+  let result = await promise
+  if (result === false) {
+    return counter(value + 1)
+  }
+  return promise
 }
-counter()
-console.log('синхронный код - выполянется первым')
+
+counter(0)
